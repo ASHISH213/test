03632477603f220f2cf04c55ef9a5526f6bac650
Rei Lin <temisrei@gmail.com>
2017-11-20 15:27:41 +0800

    add Download Youtube Feature

--- index.py
+++ index.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from pytube import YouTube
 
 app = Flask(__name__)
 
-# @app.route('/hello')
-# def hello():
-  # return 'Hello'
-
 @app.route('/')
 def index():
-  title = 'Youtube Downloader'
-  return render_template('index.html', title=title)
+    title = 'Youtube Downloader'
+    return render_template('index.html', title=title)
+
+@app.route('/submit', methods=['POST'])
+def post_submit():
+    url = request.form['inputYoutube']
+    yt = YouTube(url)
+    stream = yt.streams.first()
+    stream.download('./')
+    filename = yt.title
+    return redirect(url_for('index', filename=filename))
+
 
 if __name__ == '__main__':
-  app.run(port=9000)
+  app.run(debug=True, port=9000)
